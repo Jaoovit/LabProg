10913e4e1794330dfa68fa4d869aff4118b54780
João Oliveira <jaoovit@gmail.com>
2026-03-06 21:41:20 +0000

    validation to check roman number and create a function to convert unit values

--- roman_numeral_converter.c
+++ roman_numeral_converter.c
@@ -17,14 +17,26 @@ int validRomanNumber(char romanNumber[])
 {
     int counter = 0;
 
+    int decimalNumber = 0;
+
     for (int i = 1; i < romanNumber[i] != '\0'; i++)
     {
+        if (romanNumber[i] != "I" ||
+            romanNumber[i] != "V" ||
+            romanNumber[i] != "X" ||
+            romanNumber[i] != "L" ||
+            romanNumber[i] != "C" ||
+            romanNumber[i] != "D" ||
+            romanNumber[i] != "M")
+        {
+            return 0;
+        }
+
         if (romanNumber[i] == romanNumber[i - 1])
         {
             counter++;
             if (counter >= 3)
             {
-                printf("%s", "Invalid");
                 return 0;
             }
         }
@@ -33,12 +45,45 @@ int validRomanNumber(char romanNumber[])
             counter = 0;
         }
     }
-    printf("%s", "valid");
     return 1;
+}
+
+int decimalValue(char unit)
+{
+    switch (unit)
+    {
+    case 'I':
+        return 1;
+    case 'V':
+        return 5;
+    case 'X':
+        return 10;
+    case 'L':
+        return 50;
+    case 'C':
+        return 100;
+    case 'D':
+        return 500;
+    case 'M':
+        return 1000;
+    }
+}
+
+int convertRomanNumber(char romanNumber[])
+{
+    if (!validRomanNumber(romanNumber))
+    {
+        printf("%s", "Roman number invalid format");
+        return 0;
+    }
+
+    for (int i = 0; i < romanNumber[i] != '\0'; i++)
+    {
+    }
 }
 
 int main()
 {
-    validRomanNumber("XXXVVVIII");
+    convertRomanNumber("XXXVVVVIII");
     return 0;
 }
