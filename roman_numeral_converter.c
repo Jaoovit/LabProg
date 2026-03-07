ec25c8c0d20cf6c70ecdec53efd5a2cc04deaf2a
João Oliveira <jaoovit@gmail.com>
2026-03-07 10:41:07 +0000

    Adjust logic to add values in array to sum

--- roman_numeral_converter.c
+++ roman_numeral_converter.c
@@ -1,33 +1,42 @@
 #include <stdio.h>
+#include <string.h>
 
-/*
-Roman numbers:
- I = 1,
- V = 5,
- X = 10,
- L = 50,
- C = 100,
- D = 500,
- M = 1000
+int decimalValue(char unit)
+{
+    switch (unit)
+    {
+    case 'I':
+        return 1;
+    case 'V':
+        return 5;
+    case 'X':
+        return 10;
+    case 'L':
+        return 50;
+    case 'C':
+        return 100;
+    case 'D':
+        return 500;
+    case 'M':
+        return 1000;
+    default:
+        return 0;
+    }
+}
 
- Rules: The same number cannot appear three times in a row.
-*/
-
-int validRomanNumber(char romanNumber[])
+int validRomanNumber(char romanNumber[], int length)
 {
     int counter = 0;
 
-    int decimalNumber = 0;
-
-    for (int i = 1; i < romanNumber[i] != '\0'; i++)
+    for (int i = 1; i < length; i++)
     {
-        if (romanNumber[i] != "I" ||
-            romanNumber[i] != "V" ||
-            romanNumber[i] != "X" ||
-            romanNumber[i] != "L" ||
-            romanNumber[i] != "C" ||
-            romanNumber[i] != "D" ||
-            romanNumber[i] != "M")
+        if (romanNumber[i] != 'I' &&
+            romanNumber[i] != 'V' &&
+            romanNumber[i] != 'X' &&
+            romanNumber[i] != 'L' &&
+            romanNumber[i] != 'C' &&
+            romanNumber[i] != 'D' &&
+            romanNumber[i] != 'M')
         {
             return 0;
         }
@@ -48,42 +57,48 @@ int validRomanNumber(char romanNumber[])
     return 1;
 }
 
-int decimalValue(char unit)
+int sumArrayContent(int arr[], int length)
 {
-    switch (unit)
+    int arraySum = 0;
+
+    for (int i = 0; i < length; i++)
     {
-    case 'I':
-        return 1;
-    case 'V':
-        return 5;
-    case 'X':
-        return 10;
-    case 'L':
-        return 50;
-    case 'C':
-        return 100;
-    case 'D':
-        return 500;
-    case 'M':
-        return 1000;
+        arraySum += arr[i];
     }
+
+    return arraySum;
 }
 
 int convertRomanNumber(char romanNumber[])
 {
-    if (!validRomanNumber(romanNumber))
+    int length = strlen(romanNumber);
+
+    if (!validRomanNumber(romanNumber, length))
     {
         printf("%s", "Roman number invalid format");
         return 0;
     }
 
-    for (int i = 0; i < romanNumber[i] != '\0'; i++)
+    int arr[10] = {0};
+
+    for (int i = 0; i < length; i++)
     {
+        if (decimalValue(romanNumber[i]) < decimalValue(romanNumber[i + 1]))
+        {
+            arr[i] = -decimalValue(romanNumber[i]);
+        }
+        else
+        {
+            arr[i] = decimalValue(romanNumber[i]);
+        }
     }
+
+    int romanNumberConverted = sumArrayContent(arr, length);
+    return romanNumberConverted;
 }
 
 int main()
 {
-    convertRomanNumber("XXXVVVVIII");
+    printf("%i", convertRomanNumber("XIX"));
     return 0;
 }
